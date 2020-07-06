bf66b245ed8334889dd2bd3dfb430e716001b164
Junhyun Park <junhypar@student.42seoul.kr>
2020-07-06 17:35:00 +0900

    first finish libft

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: junhypar <junhypar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 09:36:05 by junhypar          #+#    #+#             */
-/*   Updated: 2020/07/06 14:44:57 by junhypar         ###   ########.fr       */
+/*   Updated: 2020/07/06 17:34:13 by junhypar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,13 @@
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list *ot;
-	t_list *ot2;
 
-	ot = *lst;
-	while (ot->next)
+	while (*lst)
 	{
-		ot2 = ot;
-		ot = ot->next;
-		del(ot2->content);
-		free(ot2);
-		ot2 = 0;
+		del((*lst)->content);
+		ot = *lst;
+		*lst = (*lst)->next;
+		free(ot);
 	}
-	del(ot->content);
-	free(ot);
-	ot = 0;
+	*lst = NULL;
 }
