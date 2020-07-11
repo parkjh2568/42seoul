fa01c643072af214ca4d4b29278f1344606eb051
Junhyun Park <junhypar@student.42seoul.kr>
2020-07-11 14:51:46 +0900

    ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: junhypar <junhypar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 16:14:11 by junhypar          #+#    #+#             */
-/*   Updated: 2020/07/11 14:38:18 by junhypar         ###   ########.fr       */
+/*   Updated: 2020/07/11 14:51:03 by junhypar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memchr(const void *src, int c, size_t n)
 {
-	const char		*s;
+	unsigned char		*s;
 
-	s = (const char *)src;
+	s = (unsigned char *)src;
 	while (n--)
 	{
-		if (*s++ == c)
+		if (*s++ == (unsigned char)c)
 			return ((void *)(s - 1));
 	}
 	return (0);
